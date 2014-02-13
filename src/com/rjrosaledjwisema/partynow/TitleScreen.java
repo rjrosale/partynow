a937087945f2e96671eb22902a353afd8663ba91
unknown <rjrosale@calpoly.edu>
2014-02-13 11:05:04 -0800

    Initial commit

---
+++ src/com/rjrosaledjwisema/partynow/TitleScreen.java
@@ -0,0 +1,22 @@
+package com.rjrosaledjwisema.partynow;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TitleScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_title_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.title_screen, menu);
+		return true;
+	}
+
+}
