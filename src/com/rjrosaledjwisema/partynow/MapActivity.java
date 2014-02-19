f56578954426ab3489c0b5dcad65074789a7bfbf
crater49 <cratter49@pacbell.net>
2014-02-18 23:02:59 -0800

    Got Google maps to appear but maps are blank...

--- src/com/rjrosaledjwisema/partynow/MapActivity.java
+++ src/com/rjrosaledjwisema/partynow/MapActivity.java
@@ -1,12 +1,15 @@
 package com.rjrosaledjwisema.partynow;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MapActivity extends FragmentActivity {
@@ -20,8 +23,7 @@ public class MapActivity extends FragmentActivity {
 		String address;
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		setUpMapIfNeeded();
+		initLayout();
 		
 		intentbundle = this.getIntent().getExtras();
 		if(intentbundle != null) {
@@ -31,25 +33,30 @@ public class MapActivity extends FragmentActivity {
 			}
 		}
 	}
+	
+	protected void initLayout() {
+		setContentView(R.layout.activity_map);
+		setUpMapIfNeeded();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.map_screen, menu);
-		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-		
 		return true;
 	}
 
 	private void setUpMapIfNeeded() {
 	    // Do a null check to confirm that we have not already instantiated the map.
+
 	    if (mMap == null) {
-	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-	                            .getMap();
+	    	mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+	
 	        // Check if we were successful in obtaining the map.
 	        if (mMap != null) {
 	            // The Map is verified. It is now safe to manipulate the map.
-
+	        	mMap.getUiSettings().setAllGesturesEnabled(true);
+	        	mMap.getUiSettings().setMyLocationButtonEnabled(true);
 	        }
 	    }
 	}
