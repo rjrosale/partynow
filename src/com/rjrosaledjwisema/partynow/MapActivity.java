63e4f176893c9e1ec4926e05267ad7d4f2a5646c
crater49 <cratter49@pacbell.net>
2014-02-19 14:25:11 -0800

    Added getShaKey method.

--- src/com/rjrosaledjwisema/partynow/MapActivity.java
+++ src/com/rjrosaledjwisema/partynow/MapActivity.java
@@ -1,5 +1,8 @@
 package com.rjrosaledjwisema.partynow;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -7,8 +10,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 
@@ -48,7 +56,7 @@ public class MapActivity extends FragmentActivity {
 
 	private void setUpMapIfNeeded() {
 	    // Do a null check to confirm that we have not already instantiated the map.
-
+		getShaKey();
 	    if (mMap == null) {
 	    	mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 	
@@ -60,5 +68,26 @@ public class MapActivity extends FragmentActivity {
 	        }
 	    }
 	}
+	
+	 private void getShaKey() {
+
+		 try {
+		 PackageInfo info = getPackageManager().getPackageInfo("your.package.name",
+		 PackageManager.GET_SIGNATURES);
+		 for (Signature signature : info.signatures) {
+		 MessageDigest md = MessageDigest.getInstance("SHA");
+		 md.update(signature.toByteArray());
+		 Log.v("djwisema", "KeyHash:" + Base64.encodeToString(md.digest(),
+		 Base64.DEFAULT));
+		 }
+		 } catch (NameNotFoundException e) {
+		 e.printStackTrace();
+
+		 } catch (NoSuchAlgorithmException e) {
+		 e.printStackTrace();
+
+		 }
+
+		 }
 	
 }
