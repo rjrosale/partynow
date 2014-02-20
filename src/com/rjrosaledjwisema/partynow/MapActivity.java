1e8cabef6711c778a3f11ac9123fdda24937da79
Wise <cratter49@pacbell.net>
2014-02-19 21:52:44 -0800

    Auto-location and in-progress pinning

--- src/com/rjrosaledjwisema/partynow/MapActivity.java
+++ src/com/rjrosaledjwisema/partynow/MapActivity.java
@@ -1,49 +1,68 @@
 package com.rjrosaledjwisema.partynow;
 
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationChangeListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.SyncStateContract.Constants;
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 
-public class MapActivity extends FragmentActivity {
+public class MapActivity extends FragmentActivity 
+implements OnMyLocationChangeListener {
 
 	private GoogleMap mMap;
+	private LatLng myLocation;
 	private String address = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Bundle intentbundle;
-		String address;
-		
 		super.onCreate(savedInstanceState);
 		initLayout();
-		
-		intentbundle = this.getIntent().getExtras();
-		if(intentbundle != null) {
-			address = intentbundle.getString("addr");
-			if(address != null) {
-				this.address = address;
-			}
-		}
 	}
 	
 	protected void initLayout() {
 		setContentView(R.layout.activity_map);
+		
+		Bundle intentbundle = this.getIntent().getExtras();
+		if(intentbundle != null) {
+			String addr = intentbundle.getString("addr");
+			if(addr != null) {
+				this.address = addr;
+			}
+		}
+		
 		setUpMapIfNeeded();
 	}
 
@@ -56,7 +75,7 @@ public class MapActivity extends FragmentActivity {
 
 	private void setUpMapIfNeeded() {
 	    // Do a null check to confirm that we have not already instantiated the map.
-		getShaKey();
+		
 	    if (mMap == null) {
 	    	mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 	
@@ -65,29 +84,42 @@ public class MapActivity extends FragmentActivity {
 	            // The Map is verified. It is now safe to manipulate the map.
 	        	mMap.getUiSettings().setAllGesturesEnabled(true);
 	        	mMap.getUiSettings().setMyLocationButtonEnabled(true);
+	        	mMap.setMyLocationEnabled(true);
+	        	mMap.setOnMyLocationChangeListener(this);
+	            pinEventAddress();
 	        }
 	    }
 	}
 	
-	 private void getShaKey() {
-
-		 try {
-		 PackageInfo info = getPackageManager().getPackageInfo("com.rjrosaledjwisema.partynow",
-		 PackageManager.GET_SIGNATURES);
-		 for (Signature signature : info.signatures) {
-		 MessageDigest md = MessageDigest.getInstance("SHA");
-		 md.update(signature.toByteArray());
-		 Log.v("djwisema", "KeyHash:" + Base64.encodeToString(md.digest(),
-		 Base64.DEFAULT));
-		 }
-		 } catch (NameNotFoundException e) {
-		 e.printStackTrace();
-
-		 } catch (NoSuchAlgorithmException e) {
-		 e.printStackTrace();
-
-		 }
-
-		 }
+	private void pinEventAddress() {
+		Geocoder geocoder = new Geocoder(this); 
+    	List<Address> addresses = null;
+    	
+    	try {
+			addresses = geocoder.getFromLocationName(this.address, 1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	if(addresses.size() > 0) {
+    	    double latitude = addresses.get(0).getLatitude();
+    	    double longitude = addresses.get(0).getLongitude();
+    	    
+    	    mMap.addMarker(new MarkerOptions()
+            .position(new LatLng(latitude, longitude))
+            .title("Your Event!"));
+    	}
+	}
+	
+	@Override
+	public void onMyLocationChange(Location location) {
+		// TODO Auto-generated method stub
+	
+		CameraUpdate myLoc = CameraUpdateFactory.newCameraPosition(
+	            new CameraPosition.Builder().target(new LatLng(location.getLatitude(),
+	                   location.getLongitude())).zoom(15).build());
+	    mMap.moveCamera(myLoc);
+	    mMap.setOnMyLocationChangeListener(null);
+	}
 	
 }
