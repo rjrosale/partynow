a8682ac49668b524d3b13a1ed433ab0447f64fc8
unknown <rjrosale@calpoly.edu>
2014-03-02 14:28:06 -0800

    Registration layout

--- src/com/rjrosaledjwisema/partynow/RegistrationScreen.java
+++ src/com/rjrosaledjwisema/partynow/RegistrationScreen.java
@@ -10,7 +10,7 @@ public class RegistrationScreen extends Activity{
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_screen);
+		setContentView(R.layout.registration_screen);
 		
 
 	}
